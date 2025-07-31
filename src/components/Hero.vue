09fcd9000a1632c6e8fc6f6345a8d75768cfc024
Vnz-007 <viniciusloiolajg@gmail.com>
2025-07-31 15:49:22 -0300

    Add mouse icon animated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -114,6 +114,19 @@
         </button>
       </div>
     </div>
+
+    <div
+      class="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce"
+    >
+      <div
+        class="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center"
+      >
+        <div
+          id="mouse-mg"
+          class="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"
+        ></div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -168,5 +181,9 @@ button {
 }
 button {
   padding: 0.75rem 2rem;
+}
+
+#mouse-mg {
+  margin-top: 0.5rem;
 }
 </style>
