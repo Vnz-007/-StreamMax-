b0d401a2fc24c37cb900ca4929d77ed2bae5a26c
Vnz-007 <viniciusloiolajg@gmail.com>
2025-08-02 13:23:26 -0300

    NavBar Responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,9 @@
   <header
     class="fixed w-full border-b border-gray-800 bg-black/90 backdrop-blur-md z-10"
   >
-    <div class="flex items-center justify-center h-16 gap-8">
+    <div
+      class="max-w-7xl flex items-center sm:justify-between lg:justify-center h-16 gap-8 mx-auto px-4 sm:px-6 lg:px-8"
+    >
       <div>
         <a href="#" class="flex items-center justify-center gap-2.5">
           <svg
@@ -28,7 +30,7 @@
       </div>
 
       <nav>
-        <ul class="flex items-center justify-center gap-8">
+        <ul class="sm:hidden lg:flex items-center justify-center gap-8">
           <li>
             <a
               href="#"
@@ -69,7 +71,7 @@
 
       <div
         id="input__container"
-        class="max-w-md lg:flex items-center flex-1 mx-8"
+        class="max-w-md sm:hidden lg:flex items-center flex-1 mx-8"
       >
         <div class="w-full relative">
           <svg
@@ -96,82 +98,112 @@
         </div>
       </div>
 
-      <div class="flex items-center justify-center space-x-4 gap-8">
-        <!-- <button
-          id="responsive__search-icon"
-          class="p-2 cursor-pointer sm:relative lg:hidden"
-        >
+      <div
+        class="flex items-center justify-center sm:space-x-4 lg:space-x-0 lg:gap-8"
+      >
+        <a href="#">
+          <button
+            id="responsive__search-icon"
+            class="p-2 cursor-pointer sm:flex lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-search text-gray-300 hover:text-white transition-colors"
+              id="search-icon"
+            >
+              <circle cx="11" cy="11" r="8" />
+              <line x1="21" y1="21" x2="16.65" y2="16.65" />
+            </svg>
+          </button>
+        </a>
+
+        <a href="#">
+          <button class="p-2 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="lucide lucide-bell w-5 h-5 text-gray-300 hover:text-white transition-colors"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+              <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+            </svg>
+          </button>
+        </a>
+
+        <a href="#">
+          <button class="p-2 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-heart w-5 h-5 text-gray-300 hover:text-white transition-colors"
+            >
+              <path
+                d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
+              />
+            </svg>
+          </button>
+        </a>
+
+        <a href="#">
+          <button class="p-2 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="lucide lucide-user w-5 h-5 text-gray-300 hover:text-white transition-colors"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"
+              />
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </button>
+        </a>
+
+        <button class="p-2 cursor-pointer sm:flex lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-search absolute left-3 top-1/4 transform text-gray-400"
+            class="lucide lucide-menu w-5 h-5 text-gray-300 hover:text-white transition-colors"
           >
-            <circle cx="11" cy="11" r="8" />
-            <line x1="21" y1="21" x2="16.65" y2="16.65" />
-          </svg>
-        </button> -->
-
-        <button class="p-2 cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="#none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lucide lucide-bell w-5 h-5 text-gray-300 hover:text-white transition-colors"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-            <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-          </svg>
-        </button>
-
-        <button class="p-2 cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-heart w-5 h-5 text-gray-300 hover:text-white transition-colors"
-          >
-            <path
-              d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
-            />
-          </svg>
-        </button>
-
-        <button class="p-2 cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lucide lucide-user w-5 h-5 text-gray-300 hover:text-white transition-colors"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"
-            />
-            <circle cx="12" cy="7" r="4"></circle>
+            <line x1="4" x2="20" y1="12" y2="12"></line>
+            <line x1="4" x2="20" y1="6" y2="6"></line>
+            <line x1="4" x2="20" y1="18" y2="18"></line>
           </svg>
         </button>
       </div>
