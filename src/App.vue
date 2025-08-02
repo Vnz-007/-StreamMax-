28396625a0b4cecdad46bc56a13a9e3c6c2964e6
Vnz-007 <viniciusloiolajg@gmail.com>
2025-08-02 11:51:49 -0300

    Add sections of the films in my app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <NavBar />
   <Hero />
-  <FilmsSection />
+  <ContinueWatching />
+  <PopularMovies />
+  <TrendingSeries />
+  <RecommendedAnime />
+  <ThisWeeksLaunches />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import Hero from "./components/Hero.vue";
-import FilmsSection from "./components/FilmsSection.vue";
+import ContinueWatching from "./components/ContinueWatching.vue";
+import PopularMovies from "./components/PopularMovies.vue";
+import TrendingSeries from "./components/TrendingSeries.vue";
+import RecommendedAnime from "./components/RecommendedAnime.vue";
+import ThisWeeksLaunches from "./components/ThisWeeksLaunches.vue";
 // export default {
 //   name: "app",
 //   components: {
