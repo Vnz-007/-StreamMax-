89b19d0d14c8cdd7b11afcc3256c1577eecd74dc
Vnz-007 <viniciusloiolajg@gmail.com>
2025-08-02 11:37:42 -0300

    Create a new component = FilmsSection.vue & tw ok

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <NavBar />
   <Hero />
+  <FilmsSection />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
 import Hero from "./components/Hero.vue";
+import FilmsSection from "./components/FilmsSection.vue";
 // export default {
 //   name: "app",
 //   components: {
