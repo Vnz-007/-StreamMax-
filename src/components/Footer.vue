b745868c6438eb6a33027eae9b9e8c5bae13ab1e
Vnz-007 <viniciusloiolajg@gmail.com>
2025-08-03 00:20:34 -0300

    Add links for my social media & change icons for my stack of social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,7 +32,9 @@
           </p>
           <nav class="flex items-center space-x-4 mt-6">
             <a
-              href="#"
+              href="https://github.com/Vnz-007"
+              target="_blank"
+              noreferrer="noopener"
               class="text-gray-400 hover:text-white transition-colors"
             >
               <svg
@@ -45,15 +47,18 @@
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-facebook w-5 h-5"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
               >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path
-                  d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
-                ></path>
+                  d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+                />
               </svg>
             </a>
             <a
-              href="#"
+              href="https://www.linkedin.com/in/vin%C3%ADcius-loiola-bb8688341"
+              target="_blank"
+              noreferrer="noopener"
               class="text-gray-400 hover:text-white transition-colors"
             >
               <svg
@@ -66,38 +71,48 @@
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-twitter w-5 h-5"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin"
               >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M8 11v5" />
+                <path d="M8 8v.01" />
+                <path d="M12 16v-5" />
+                <path d="M16 16v-3a2 2 0 1 0 -4 0" />
                 <path
-                  d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"
-                ></path>
+                  d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z"
+                />
               </svg>
             </a>
             <a
-              href="#"
+              href="https://www.instagram.com/_vnz.007_?igsh=ZmVybWNrN2R1eXI1"
+              target="_blank"
+              noreferrer="noopener"
               class="text-gray-400 hover:text-white transition-colors"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
+                width="26"
+                height="26"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-instagram w-5 h-5"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
               >
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path
-                  d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
-                ></path>
-                <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
+                  d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"
+                />
+                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                <path d="M16.5 7.5v.01" />
               </svg>
             </a>
             <a
-              href="#"
+              href="mailto:viniciusloiolajg@gmail.com"
+              target="_blank"
+              noreferrer="noopener"
               class="text-gray-400 hover:text-white transition-colors"
             >
               <svg
@@ -110,12 +125,13 @@
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-youtube w-5 h-5"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-mail"
               >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path
-                  d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"
-                ></path>
-                <path d="m10 15 5-3-5-3z"></path>
+                  d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"
+                />
+                <path d="M3 7l9 6l9 -6" />
               </svg>
             </a>
           </nav>
